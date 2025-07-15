a6029b6f80243384d86db04a73e1f691645feed1
La Digue Records <ladiguerecords@gmail.com>
2025-07-15 16:45:52 +0200

    Add files via upload

--- ladigue.js
+++ ladigue.js
@@ -4,7 +4,6 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 $(document).ready(function () {
   //video back
- //video back
   jQuery('[data-vbg]').youtube_background({
     'volume': 1,
     'muted': false,
@@ -12,9 +11,6 @@ $(document).ready(function () {
     'end-at': 0,
     'mobile': true
   });
- /* const firstElement = $('[data-vbg]')[0];
-  const firstInstance = VIDEO_BACKGROUNDS.get(firstElement);
-	console.log(firstElement);*/
   const myback = jQuery('#back');
   myback.on('video-background-ready', function (event) {
     $('#back').animate({
@@ -22,13 +18,21 @@ $(document).ready(function () {
     }, 3000);
   });
   //web artists
-  $('.web, .bc, .rs, .itemblocVG, .itemblocV, .itemblocM').on('click', function (e) {
+  $('.web, .bc, .rs, .itembloc').on('click', function (e) {
     e.preventDefault();
     if ($(this).attr("adresse") != "null") {
       window.open($(this).attr("adresse"), '_blank');
+    } else {
+      scrollto($(this).attr("cible"));
     }
   })
-});
+  $(window).on('scroll', checkscroll);
+  $(document).on('click', '.top', function (e) {
+    e.preventDefault();
+    scrollto('main');
+  });
+
+})
 /////////////////////////////////////////////////////////////// Les fonctions
 function viewvideo() {
   $('#back').animate({
@@ -36,3 +40,33 @@ function viewvideo() {
   }, 3000);
 }
 
+function checkscroll(e) {
+  var scrollTop = $(window).scrollTop(); // position actuelle du scroll
+  var windowHeight = $(window).height(); // hauteur de la fenêtre
+  var targetOffset = $('#section3').offset().top; // position de la div
+
+  if (targetOffset <= scrollTop + $('#section1').height() + 80) {
+    $('#section1').css({
+      position: 'static'
+    });
+  } else {
+    $('#section1').css({
+      position: 'sticky',
+      'top': 0,
+    });
+  }
+}
+
+function scrollto(lacible) {
+  if ($(window).width() > 600) {
+    $("body, html").animate({
+        scrollTop: $('#' + lacible).offset().top - 80,
+      },
+      1000);
+  } else {
+    $("body, html").animate({
+        scrollTop: $('#' + lacible).offset().top,
+      },
+      1000);
+  }
+}
